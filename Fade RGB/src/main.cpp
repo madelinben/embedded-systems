4e0aa45281b6651dfdbd6a9c58174c92be8ff488
madelinben <ben.madelin@ntlworld.com>
2022-02-22 17:33:20 +0000

    Established pin configuration.

--- Fade RGB/src/main.cpp
+++ Fade RGB/src/main.cpp
@@ -1,5 +1,18 @@
 #include <Arduino.h>
 
+// GPIO Configuration
+const int RED_PIN = 12;
+const int GREEN_PIN = 13;
+const int BLUE_PIN = 14;
+
+// PWM Properties
+const int RED_CHANNEL = 0;
+const int GREEN_CHANNEL = 1;
+const int BLUE_CHANNEL = 2;
+
+const int PWM_FREQ = 5000;
+const int PWM_RES = 8;
+
 void setup() {
   // put your setup code here, to run once:
 }
